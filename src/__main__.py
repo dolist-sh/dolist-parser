f112e29a35927002f6619bccc0f9730b8b121b93
yunjae <yunjae.oh.nl@gmail.com>
2022-07-17 10:23:20 +0200

    build: scaffolding of inspecting method

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,28 @@
+import os
 from parser import parse
 
+def inspect_source(path: str):
+    dir_list = os.listdir(path)
+
+    print("-------------------")
+    print("Start inspecting the source code")
+    print(dir_list)
+
+    for content in dir_list:
+        current_path = f"{path}/{content}"
+
+        if (os.path.isdir(current_path)) is True:
+            print(f"Calling itself again for a folder: {current_path}")
+            inspect_source(current_path)
+        else:
+            print(f"Calling parse for a file: {current_path}")
+
+
+    
+
 if __name__ == "__main__":
-    parse("input/index.ts")
+
+    pwd = os.getcwd()
+    inspect_source(f"{pwd}/input")
+
+
