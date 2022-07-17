5b991c54d60ba0aaecad39dc924679088621d1fe
yunjae <yunjae.oh.nl@gmail.com>
2022-07-17 14:21:59 +0200

    refactor: remove comments

--- src/main.py
+++ src/main.py
@@ -11,25 +11,13 @@ def run(path: str) -> List[ParsedComment]:
         result = []
         dir_list = os.listdir(path)
 
-        # print("-------------------")
-        # print(f"Start inspecting")
-        # print(dir_list)
-
         for content in dir_list:
             current_path = f"{path}/{content}"
-            # print(f"Current path: {current_path}")
 
             if (os.path.isdir(current_path)) is True:
-                # print(f"Calling itself again for a folder: {current_path}")
                 inspect_source(current_path)
             else:
-                # print(f"Calling parse for a file: {current_path}")
                 result = result + parse(current_path)
-
-        # print("--------------------------------------------")
-        # print("inspect_func call being returned")
-        # print(f"Output length being returned: {len(result)}")
-        # print("--------------------------------------------")
 
         return output.extend(result)
 
