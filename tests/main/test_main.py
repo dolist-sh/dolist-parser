4abe645d1c2c8573359ca7512c780a9b623a849a
Yunjae Oh <yunjae@Yunjaes-MacBook-Pro.local>
2022-07-18 19:17:58 +0200

    build: handle multiple language at run

--- tests/main/test_main.py
+++ tests/main/test_main.py
@@ -8,6 +8,6 @@ pwd = getcwd()
 def test_source_parsing():
     """Should parse the 53 to-do comments from source"""
 
-    result = run(f"{pwd}/tests/main/data")
+    result = run(f"{pwd}/tests/main/src")
 
-    assert len(result) == 53
+    assert len(result) == 71
--- tests/parser/js/test_js_parser.py
+++ tests/parser/js/test_js_parser.py
@@ -1,20 +1,28 @@
 import pytest
 from os import getcwd
-from src.parsers.js import parse
+from src.parsers.javascript import JSParser
 
 pwd = getcwd()
+
+JS_ONELINE_PATTERN = r"//"
+JS_MULTILINE_OPEN_PATTERN = r"/\*"
+JS_MULTILINE_CLOSE_PATTERN = r"\*/"
+
+js_parser = JSParser(
+    JS_ONELINE_PATTERN, JS_MULTILINE_OPEN_PATTERN, JS_MULTILINE_CLOSE_PATTERN
+)
 
 
 def test_parser_typescript():
     """Should parse 11 to-dos from input file"""
-    result = parse(f"{pwd}/tests/parser/js/src/typescript.ts")
+    result = js_parser.parse(f"{pwd}/tests/parser/js/src/typescript.ts")
 
     assert len(result) == 11
 
 
 def test_parser_javascript():
     """Should parse 7 to-dos from input file"""
-    result = parse(f"{pwd}/tests/parser/js/src/javascript.js")
+    result = js_parser.parse(f"{pwd}/tests/parser/js/src/javascript.js")
 
     assert len(result) == 7
 
