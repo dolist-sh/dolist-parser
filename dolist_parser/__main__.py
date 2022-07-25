36e855d4c5e631de4c47c51b7dfdf2629452d005
yunjae <yunjae.oh.nl@gmail.com>
2022-07-25 13:20:23 +0200

    refactor: simplify main

--- dolist_parser/__main__.py
+++ dolist_parser/__main__.py
@@ -1,7 +1,43 @@
+import re
 import os
 import json
-from dolist_parser import run
+
+from dolist_parser import js_parser, py_parser
 from utils import write_to_json
+
+from definition import ParsedComment
+from typing import List
+
+
+def run(path: str) -> List[ParsedComment]:
+    output = []
+
+    def inspect_source(path: str) -> List[ParsedComment]:
+        result = []
+        dir_list = os.listdir(path)
+
+        for content in dir_list:
+            current_path = f"{path}/{content}"
+
+            if (os.path.isdir(current_path)) is True:
+                inspect_source(current_path)
+            else:
+                parsed = []
+
+                if re.search(r"(\.js$|\.ts$)", content, re.IGNORECASE):
+                    parsed = js_parser.parse(current_path)
+
+                if re.search(r"(\.py$)", content, re.IGNORECASE):
+                    parsed = py_parser.parse(current_path)
+
+                result = result + parsed
+
+        return output.extend(result)
+
+    inspect_source(path)
+
+    return output
+
 
 if __name__ == "__main__":
 
