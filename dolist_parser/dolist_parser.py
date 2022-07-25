77032603dc0edb26d56af8f715348bf765b7cbb8
yunjae <yunjae.oh.nl@gmail.com>
2022-07-25 13:26:30 +0200

    fix: failing main test

--- dolist_parser/dolist_parser.py
+++ dolist_parser/dolist_parser.py
@@ -1,7 +1,13 @@
 """ Export language parsers """
 
+import re
+import os
+
 from parsers.javascript import JSParser
 from parsers.python import PYParser
+
+from definition import ParsedComment
+from typing import List
 
 JS_ONELINE_PATTERN = r"//"
 JS_MULTILINE_OPEN_PATTERN = r"/\*"
@@ -14,3 +20,33 @@ js_parser = JSParser(
     JS_ONELINE_PATTERN, JS_MULTILINE_OPEN_PATTERN, JS_MULTILINE_CLOSE_PATTERN
 )
 py_parser = PYParser(PY_ONELINE_PATTERN, PY_MULTILINE_PATTERN)
+
+
+def run(path: str) -> List[ParsedComment]:
+    output = []
+
+    def inspect_source(path: str) -> List[ParsedComment]:
+        result = []
+        dir_list = os.listdir(path)
+
+        for content in dir_list:
+            current_path = f"{path}/{content}"
+
+            if (os.path.isdir(current_path)) is True:
+                inspect_source(current_path)
+            else:
+                parsed = []
+
+                if re.search(r"(\.js$|\.ts$)", content, re.IGNORECASE):
+                    parsed = js_parser.parse(current_path)
+
+                if re.search(r"(\.py$)", content, re.IGNORECASE):
+                    parsed = py_parser.parse(current_path)
+
+                result = result + parsed
+
+        return output.extend(result)
+
+    inspect_source(path)
+
+    return output
