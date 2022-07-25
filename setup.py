fccbcc51207a4245a9da3db75e985c8cd01fe67d
yunjae <yunjae.oh.nl@gmail.com>
2022-07-25 13:22:41 +0200

    chore: remove whitespace

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Parser for dolist",
     url="git@github.com:whathecker/comment-parser.git",
     author="Yunjae Oh",
-    author_email="yunjae.oh.nl@gmail.com    ",
+    author_email="yunjae.oh.nl@gmail.com",
     license="unlicense",
     packages=setuptools.find_packages(),
     zip_safe=False,
